54b8f11f2caa62dc85a39199f89c408803b00fe6
Maksim Orekhov <timberwolf.max@gmail.com>
2017-12-16 17:53:25 +0300

    начат адаптив

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,6 +22,7 @@ if (document.getElementById('hamburger')) {
 
 // переворот формы авторизации на главной странице
 
+if (document.querySelector('.auth__button')) {
 const authButton = document.querySelector('.auth__button');
 const rotateContainer = document.querySelector('.rotate__container');
 const backButton = document.querySelector('.button__back');
@@ -34,4 +35,69 @@ authButton.onclick = function() {
 backButton.onclick = function() {
   rotateContainer.classList.remove('active');
   authButton.classList.remove('auth__button-hidden');
+}
+}
+
+//подключение гугл-карты
+if (document.getElementById('map')) {
+const center = {lat: 53.2050, lng: 34.4177};
+const home = {lat: 53.2051, lng: 34.4215}
+const map = new google.maps.Map(document.getElementById('map'), {
+  center: center,
+  zoom: 17,
+  draggable: false,
+  styles: [
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#61dac9"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 80
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+]
+});
+
+const marker = new google.maps.Marker({
+  position: home,
+  map: map,
+  animation: google.maps.Animation.DROP,
+});
+}
+
+// работа футера на странице about
+if (document.querySelector('.container__about')) {
+    const footer = document.querySelector('.footer')
+    footer.classList.add('footer__about');
 }
